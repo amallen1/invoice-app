c1b276b2023bfe7aecdfdb30a3080e30509940ad
amallen1 <amallen046@gmail.com>
2024-02-19 15:01:39 -0600

    added dropdown menus

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import FormItem from "./FormItem";
+import Image from "next/image";
 
 const Form = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="">
       <form action="">
@@ -63,34 +66,86 @@ const Form = () => {
             </div>
           </div>
 
-          <div className="mb-12">
-            <div className="mb-6">
+          <div className="mb-12 md:flex md:flex-wrap md:gap-4">
+            <div className="mb-6 flex-1 relative">
               <label htmlFor="">Invoice Date</label>
-              <input type="date" className="pr-4" />
+              <input type="date" data-date-format="DD MMMM YYYY" />
             </div>
-            <div className="mb-6">
+            <div className="mb-6 flex-1">
               <label htmlFor="">Payment Terms</label>
-              <input type="text" />
+
+              <div
+                className="dropdown-container cursor-pointer relative flex items-center justify-between"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <p>Net 1 day</p>
+
+                {isOpen && (
+                  <div className="bg-formInputColor w-full absolute left-0 top-14 rounded-md drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+                    <ul>
+                      <li className="pl-4 py-4 border-b-[1px] border-[#DFE3FA] dark:border-[#1E2139] hover:text-lightPurple">
+                        <p>Net 1 Day</p>
+                      </li>
+                      <li className="pl-4 py-4 border-b-[1px] border-[#DFE3FA] dark:border-[#1E2139] hover:text-lightPurple">
+                        <p>Net 7 Days</p>
+                      </li>
+                      <li className="pl-4 py-4 border-b-[1px] border-[#DFE3FA] dark:border-[#1E2139] hover:text-lightPurple">
+                        <p>Net 14 Days</p>
+                      </li>
+                      <li className="pl-4 py-4 border-b-[1px] border-[#DFE3FA] dark:border-[#1E2139] hover:text-lightPurple">
+                        <p>Net 30 Days</p>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+
+                <Image
+                  src="/icon-arrow-down.svg"
+                  width={10}
+                  height={10}
+                  alt="Arrow down"
+                />
+              </div>
             </div>
-            <div className="mb-6">
+            <div className="mb-6 md:w-full">
               <label htmlFor="">Project Description</label>
               <input type="text" />
             </div>
           </div>
 
           <div>
-            <p className="text-[18px] font-bold text-[#777f98] mb-6">Item List</p>
+            <p className="text-[18px] font-bold text-[#777f98] mb-6">
+              Item List
+            </p>
             {/* TODO: render list of form items */}
             <FormItem />
+            <div>
+              <button className="bg-addItem py-4 w-full rounded-[24px] text-sm_variant text-formLabel">
+                + Add New Item
+                {/* TODO: create another form item once clicked */}
+              </button>
+            </div>
           </div>
         </div>
 
         <div className="sticky bottom-0">
-          <div className="h-[64px] bg-gradient-to-t from-black opacity-20"></div>
-          <div className="bg-white dark:bg-base px-6 py-8 flex justify-between rounded-br-[20px]">
-            {/* <button onClick={() => closeModal()}>Discard</button> */}
-            <button>Save as Draft</button>
-            <button>Save & Send</button>
+          <div className="h-[64px] bg-gradient-to-t from-black opacity-20" />
+          <div className="bg-white dark:bg-base px-6 py-8 rounded-br-[20px] flex justify-center md:justify-between ">
+            {/* If creating a new invoice, Show Discard, Save as Draft, and Save & Send*/}
+            <button className="text-sm_variant text-mediumBlue dark:text-gray bg-paleGray dark:bg-lighterDarkBlue p-4 rounded-3xl">
+              Discard
+            </button>
+
+            <div className="">
+              <button className="text-sm_variant text-gray bg-[#373B53] p-4 rounded-3xl mr-2 md:mr-4">
+                Save as Draft
+              </button>
+              <button className="text-sm_variant text-white bg-lightPurple p-4 rounded-3xl">
+                Save & Send
+              </button>
+            </div>
+
+            {/* If editing an invoice, show Cancel and Save Changes buttons */}
           </div>
         </div>
       </form>
--- src/app/components/invoices/InvoiceHeader.tsx
+++ src/app/components/invoices/InvoiceHeader.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Button from "../Button";
 
 const InvoiceHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex justify-between pb-[102px] text-primary">
       <div>
@@ -10,8 +13,11 @@ const InvoiceHeader = () => {
         <span className="text-body_variant text-secondary">No invoices</span>
       </div>
 
-      <div className="flex items-center">
-        <div className="flex gap-1.5 mr-4 items-center">
+      <div className="flex items-center relative">
+        <button
+          className="flex gap-1.5 mr-4 items-center relative"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <p className="text-sm_variant">
             Filter <span className="hidden md:inline">by status</span>
             {/* TODO: Finish the dropdown menu */}
@@ -22,7 +28,32 @@ const InvoiceHeader = () => {
             height="6"
             alt="Filter Arrow Icon"
           />
-        </div>
+        </button>
+
+        {isOpen && (
+          <div className="bg-formInputColor w-[192px] px-6 py-6 absolute -left-12 top-14 rounded-md drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+            <ul>
+              <li className="flex item-center mb-2 hover:text-lightPurple">
+                <input type="checkbox" id="scales" name="scales" />
+                <label htmlFor="scales" className="ml-2 text-sm text-primary">
+                  Draft
+                </label>
+              </li>
+              <li className="flex item-center mb-2 hover:text-lightPurple">
+                <input type="checkbox" id="scales" name="scales" />
+                <label htmlFor="scales" className="ml-2 text-sm text-primary">
+                  Pending
+                </label>
+              </li>
+              <li className="flex item-center mb-2 hover:text-lightPurple">
+                <input type="checkbox" id="scales" name="scales" />
+                <label htmlFor="scales" className="ml-2 text-sm text-primary">
+                  Paid
+                </label>
+              </li>
+            </ul>
+          </div>
+        )}
         <Button />
       </div>
     </div>
